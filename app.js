195dfb738db56e3135f1d582c616f2984f6bf210
John <johnm589@yahoo.com>
2016-06-27 13:55:55 -0700

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+var
+
+    express = require ("express"),
+    dateTime = new Date,
+    utcDate= dateTime.toUTCString(),
+    port = 3000,
+    app = express();
+
+
+    app.set('view engine', 'ejs');
+
+
+    // index page
+    app.get('/', function(req, res) {
+        res.render('pages/home');
+    });
+
+    // about page
+    app.get('/about', function(req, res) {
+        res.render('pages/about');
+    });
+    app.get('/hearts', function(req, res) {
+        res.render('pages/hearts');
+    });
+    app.get('/insulter', function(req, res) {
+        res.render('pages/insulter');
+    });
+
+
+    app.listen(3000);
+    console.log('goin down on port 3000');
