102c9668287a49dc91c6cd0a159a547f49c9e3bc
John <johnm589@yahoo.com>
2016-06-27 14:06:51 -0700

    added port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var
     express = require ("express"),
     dateTime = new Date,
     utcDate= dateTime.toUTCString(),
-    port = 3000,
+    port = process.env.PORT || 3000;
     app = express();
 
 
